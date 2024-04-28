8b202efdd556f469f4ea73af336664619411976f
queeenraa <lauraamelia50@gmail.com>
2024-04-28 20:15:00 +0700

    sekarang ada filenya

---
+++ python-craft/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'python-craft',
+  webDir: 'www'
+};
+
+export default config;
