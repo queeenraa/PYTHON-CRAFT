0bc21d84d566054762b710274b18da30e04e1cd2
RivaldiDev <ripaldialdo357@gmail.com>
2024-05-05 21:27:39 +0700

    update version angular import swiper

--- python-craft/src/app/home/home.module.ts
+++ python-craft/src/app/home/home.module.ts
@@ -11,3 +11,18 @@ import { HomePageRoutingModule } from './home-routing.module';
   declarations: [HomePage],
 })
 export class HomePageModule {}
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  ...
+});
