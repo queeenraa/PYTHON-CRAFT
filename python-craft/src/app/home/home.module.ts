74b15d22f7cd94fc469b2dec278ef39fd8cf433a
RivaldiDev <ripaldialdo357@gmail.com>
2024-05-10 09:30:03 +0700

    tambah swiper di home

--- python-craft/src/app/home/home.module.ts
+++ python-craft/src/app/home/home.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -6,9 +7,14 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
   declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
 
@@ -17,7 +23,7 @@ export class HomePageModule {}
 // import { Navigation, Pagination } from 'swiper/modules';
 // // import Swiper and modules styles
 // import 'swiper/css';
-// import 'swiper/css/navigation';
+// import 'swiper/css/navigati  on';
 // import 'swiper/css/pagination';
 
 // // init Swiper:
