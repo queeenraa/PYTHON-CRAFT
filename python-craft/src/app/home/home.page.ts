50704144937e79635f5345745f12734af816a2c2
RivaldiDev <ripaldialdo357@gmail.com>
2024-06-14 15:51:38 +0700

    getting-started

--- python-craft/src/app/home/home.page.ts
+++ python-craft/src/app/home/home.page.ts
@@ -1,24 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition(':enter', [
+        // called when the element enters (added to the DOM)
+        animate(
+          '400ms ease-in',
+          style({
+            opacity: 1,
+          })
+        ),
+      ]),
+      transition(':leave', [
+        // called when the element leaves (removed from the DOM)
+        animate(
+          '400ms ease-out',
+          style({
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  isLastSlide: boolean = false;
+
   constructor() {}
 
-  slideNext() {
-    const swiper = document.querySelector('swiper-container') as any;
-    if (swiper) {
-      swiper.swiper.slideNext();
+  ngAfterViewInit() {
+    const swiperContainer = document.querySelector('swiper-container') as any;
+    if (swiperContainer && swiperContainer.swiper) {
+      swiperContainer.swiper.on('slideChange', () => {
+        this.onSlideChange(swiperContainer.swiper);
+      });
     }
   }
 
-  slidePrev() {
-    const swiper = document.querySelector('swiper-container') as any;
-    if (swiper) {
-      swiper.swiper.slidePrev();
+  slideNext() {
+    const swiperContainer = document.querySelector('swiper-container') as any;
+    if (swiperContainer && swiperContainer.swiper) {
+      swiperContainer.swiper.slideNext();
     }
+  }
+
+  skipToEnd() {
+    const swiperContainer = document.querySelector('swiper-container') as any;
+    if (swiperContainer && swiperContainer.swiper) {
+      swiperContainer.swiper.slideTo(swiperContainer.swiper.slides.length - 1);
+    }
+  }
+
+  onSlideChange(swiper: any) {
+    this.isLastSlide = swiper.isEnd;
   }
 }
