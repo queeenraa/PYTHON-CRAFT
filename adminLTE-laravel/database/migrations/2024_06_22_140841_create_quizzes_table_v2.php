0c649ce81fa10c15677040b33072b232285f852e
queeenraa <lauraamelia50@gmail.com>
2024-06-22 21:15:58 +0700

    quiz don

--- adminLTE-laravel/database/migrations/2024_06_22_140841_create_quizzes_table_v2.php
+++ adminLTE-laravel/database/migrations/2024_06_22_140841_create_quizzes_table_v2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuizzesTable extends Migration
+class CreateQuizzesTableV2 extends Migration
 {
     public function up()
     {
