e5ec60081780ed295d598a4c6862722294eb67e6
queeenraa <lauraamelia50@gmail.com>
2024-06-24 14:37:15 +0700

    login

--- adminLTE-laravel/resources/views/login.blade.php
+++ adminLTE-laravel/resources/views/login.blade.php
@@ -57,12 +57,12 @@
       </div>
       <!-- /.social-auth-links -->
 
-      <p class="mb-1">
+      {{-- <p class="mb-1">
         <a href="forgot-password.html">I forgot my password</a>
       </p>
       <p class="mb-0">
         <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
+      </p> --}}
     </div>
     <!-- /.card-body -->
   </div>
