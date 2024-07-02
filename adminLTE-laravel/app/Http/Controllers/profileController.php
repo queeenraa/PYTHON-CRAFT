c6d6e47bee234ab057fb3e61d5677858cc75ce73
FarhanArv <farhanarvan16@gmail.com>
2024-07-02 19:50:25 +0700

    ssss

--- adminLTE-laravel/app/Http/Controllers/profileController.php
+++ adminLTE-laravel/app/Http/Controllers/profileController.php
@@ -23,11 +23,44 @@ class profileController extends Controller
     {
         // return view('layouts.profile');
 
-        $profiles = User::all(); // Mengambil semua data courses dari model Course
+        // $profiles = User::all();  // Mengambil semua data courses dari model Course
+        $user = User::findOrFail($id);
 
         return view('layouts.profile.editProfile', [
             'profiles' => $profiles, // Mengirimkan data courses ke view
         ]);
+            // Mengambil data pengguna berdasarkan ID
+        $user = User::findOrFail($id);
+
+        // Mengirim data pengguna ke view
+        return view('layouts.profile.editProfile', compact('user'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('layouts.profile.editProfile', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    // Validasi input
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|max:255',
+        'role' => 'required|string|max:255',
+    ]);
+
+    // Temukan pengguna berdasarkan ID dan perbarui data
+    $user = User::findOrFail($id);
+    $user->name = $request->input('name');
+    $user->email = $request->input('email');
+    $user->role = $request->input('role');
+    $user->save();
+
+    return redirect()->route('profile.index')->with('success', 'Profile updated successfully');
     }
     
     public function destroy($id)
