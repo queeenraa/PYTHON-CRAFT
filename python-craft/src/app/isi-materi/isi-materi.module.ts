f12037019cb1f7f4e20cd508eb6e678cfdab4f4a
RivaldiDev <ripaldialdo357@gmail.com>
2024-07-05 12:02:21 +0700

    isi materi don

--- python-craft/src/app/isi-materi/isi-materi.module.ts
+++ python-craft/src/app/isi-materi/isi-materi.module.ts
@@ -1,20 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { IsiMateriPageRoutingModule } from './isi-materi-routing.module';
-
 import { IsiMateriPage } from './isi-materi.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    IsiMateriPageRoutingModule
-  ],
-  declarations: [IsiMateriPage]
+  imports: [CommonModule, FormsModule, IonicModule, IsiMateriPageRoutingModule],
+  declarations: [IsiMateriPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class IsiMateriPageModule {}
